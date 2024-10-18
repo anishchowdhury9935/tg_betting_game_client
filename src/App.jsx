e28164b8bcfdf7b4db3818401624767dccdfe7b6
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-10-19 01:40:52 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import {
   Route,
 } from "react-router-dom";
 import Rps_routes from './routes/Rps_routes.jsx';
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import socketEvents from '../games/rps/utils/socketFiles/socketEvents.js';
+import Rps_home from '../games/rps/components/Rps_home.jsx'
 function App() {
   return (
     <>
       <Router>
         {/* Same as */}
-        <Toaster/>
+        <Toaster />
         <Routes>
-          {Rps_routes().map((element) => {
+          {/* {Rps_routes().map((element) => {
             return element
-          })}
-          <Route path='*' element={<h4>page not found</h4>}/> 
+          })} */}
+          <Route path="/rps/:bettingId/:type/:game/:userId" element={<Rps_home />} />
+          <Route path='*' element={<h4>page not found</h4>} />
         </Routes>
       </Router>
     </>
