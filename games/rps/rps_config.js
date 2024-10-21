a6a54d653a338258c237de4bee26c5be0d315a33
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-10-21 18:45:40 +0530

    save

--- games/rps/rps_config.js
+++ games/rps/rps_config.js
@@ -1,7 +1,7 @@
 import config from "../../config";
 const { devModeOn } = config;
 const rps_config = {
-    API_BASE_URL_MAIN: devModeOn ? false : 'https://nomoonsol.com/api/bettingbasicdata',
+    API_BASE_URL_MAIN: devModeOn ? false : 'https://nomoonsol.com',
     API_BASE_URL_DEV: devModeOn ? 'http://localhost:5010' : false,
 }
 
